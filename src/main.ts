9540c13d37621762ac59983903c4cb9ff7a3d1f8
Dante Arias <danteariastarifa@gmail.com>
2024-03-26 21:45:09 -0400

    Update CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      origin: 'https://instituto-etc.netlify.app',
+    },
   });
 
   const config = new DocumentBuilder()
