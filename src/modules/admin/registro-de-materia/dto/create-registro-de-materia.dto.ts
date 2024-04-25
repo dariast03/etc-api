217e8372a209a9fccd87b17deb656219e93382be
Dante Arias Tarifa <danteariastarifa@gmail.com>
2024-04-25 14:45:02 -0400

    Refactor DTO for creating RegistroDeMateria to include additional properties

--- src/modules/admin/registro-de-materia/dto/create-registro-de-materia.dto.ts
+++ src/modules/admin/registro-de-materia/dto/create-registro-de-materia.dto.ts
@@ -1,1 +1,25 @@
-export class CreateRegistroDeMateriaDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRegistroDeMateriaDto {
+  @ApiProperty({
+    description: 'Información del usuario asociado al estudiante',
+  })
+  nota: number;
+
+  @ApiProperty({
+    description: 'Información del usuario asociado al estudiante',
+  })
+  estado: string;
+
+  @ApiProperty({
+    description: 'Información de la especializacion asociado al estudiante',
+    example: 'Idiomas',
+  })
+  idInscripcion: string;
+
+  @ApiProperty({
+    description: 'Información de la especializacion asociado al estudiante',
+    example: 'Idiomas',
+  })
+  idOfertaMateria: string;
+}
--- src/modules/admin/registro-de-materia/registro-de-materia.controller.ts
+++ src/modules/admin/registro-de-materia/registro-de-materia.controller.ts
@@ -7,17 +7,21 @@ import {
   Param,
   Delete,
   Query,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { RegistroDeMateriaService } from './registro-de-materia.service';
 import { CreateRegistroDeMateriaDto } from './dto/create-registro-de-materia.dto';
 import { UpdateRegistroDeMateriaDto } from './dto/update-registro-de-materia.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PrismaService } from '@/prisma.service';
 
 @Controller('registro-de-materia')
 @ApiTags('ADMIN Registro Materia')
 export class RegistroDeMateriaController {
   constructor(
     private readonly registroDeMateriaService: RegistroDeMateriaService,
+    private readonly prismaService: PrismaService,
   ) {}
 
   @Post()
@@ -25,6 +29,25 @@ export class RegistroDeMateriaController {
     @Body() createRegistroDeMateriaDto: CreateRegistroDeMateriaDto,
     @Query('crudQuery') crudQuery: string,
   ) {
+    const existRegistroInSameMateriaAndSameOffer =
+      await this.prismaService.registroDeMateria.findFirst({
+        where: {
+          idOfertaMateria: createRegistroDeMateriaDto.idOfertaMateria,
+          idInscripcion: createRegistroDeMateriaDto.idInscripcion,
+          estado: {
+            not: {
+              equals: 'ABANDONADO',
+            },
+          },
+        },
+      });
+
+    if (existRegistroInSameMateriaAndSameOffer)
+      throw new HttpException(
+        'El estudiante ya tiene un registro en esta materia y en la misma oferta',
+        HttpStatus.BAD_REQUEST,
+      );
+
     const created = await this.registroDeMateriaService.create(
       createRegistroDeMateriaDto,
       { crudQuery },
