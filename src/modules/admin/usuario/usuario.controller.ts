20059640b6910f685d0d34b2e19b1f2ed9a6950a
Dante Arias Tarifa <danteariastarifa@gmail.com>
2024-04-28 03:37:05 -0400

    Refactor user creation and update logic in UsuarioController

--- src/modules/admin/usuario/usuario.controller.ts
+++ src/modules/admin/usuario/usuario.controller.ts
@@ -7,26 +7,51 @@ import {
   Param,
   Delete,
   Query,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PrismaService } from '@/prisma.service';
+import { hash } from 'bcrypt';
 
 @Controller('usuario')
 @ApiTags('ADMIN Usuario')
 export class UsuarioController {
-  constructor(private readonly usuarioService: UsuarioService) {}
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Post()
   async create(
     @Body() createUsuarioDto: CreateUsuarioDto,
     @Query('crudQuery') crudQuery: string,
   ) {
-    const created = await this.usuarioService.create(createUsuarioDto, {
-      crudQuery,
+    const userInDb = await this.prismaService.usuario.findFirst({
+      where: { correo: createUsuarioDto.correo },
     });
-    return created;
+
+    if (userInDb) {
+      throw new HttpException(
+        'Ya existe un usuario con el mismo correo',
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const user = await this.prismaService.usuario.create({
+      data: {
+        ...createUsuarioDto,
+        contrasena: await hash(createUsuarioDto.contrasena, 10),
+      },
+    });
+
+    /*  const created = await this.usuarioService.create(createUsuarioDto, {
+      crudQuery,
+    }); */
+    return user;
   }
 
   @Get()
@@ -50,9 +75,30 @@ export class UsuarioController {
     @Body() updateUsuarioDto: UpdateUsuarioDto,
     @Query('crudQuery') crudQuery: string,
   ) {
+    const userInDb = await this.prismaService.usuario.findFirst({
+      where: { id },
+    });
+
+    if (!userInDb) {
+      throw new HttpException('user_not_found', HttpStatus.NOT_FOUND);
+    }
+
+    if (updateUsuarioDto.contrasena) {
+      updateUsuarioDto.contrasena = await hash(updateUsuarioDto.contrasena, 10);
+    }
+
+    if (updateUsuarioDto.contrasena == '') {
+      delete updateUsuarioDto.contrasena;
+    }
+
+    console.log({
+      updateUsuarioDto,
+    });
+
     const updated = await this.usuarioService.update(id, updateUsuarioDto, {
       crudQuery,
     });
+
     return updated;
   }
 
